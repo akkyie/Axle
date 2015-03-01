178bfb72e877d0ee65fc807b48b70779c1eecf34
Akkyie <akio@prioris.org>
2015-03-01 10:05:58 +0900

    Added build configuration for dynamic library

---
+++ Axle (OS X)/Axle.h
@@ -0,0 +1,12 @@
+//
+//  Axle.h
+//  Axle
+//
+//  Created by Akio Yasui on 3/1/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double Axle_VersionNumber;
+FOUNDATION_EXPORT const unsigned char Axle_VersionString[];

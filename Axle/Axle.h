0314aaae55c81676d153a735d20ad5775abb7e6f
Akio Yasui <akkyie01@gmail.com>
2015-11-09 13:12:03 +0900

    Initial Commit

---
+++ Axle/Axle.h
@@ -0,0 +1,19 @@
+//
+//  Axle.h
+//  Axle
+//
+//  Created by Akio Yasui on 11/9/15.
+//  Copyright © 2015 Akio Yasui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Axle.
+FOUNDATION_EXPORT double AxleVersionNumber;
+
+//! Project version string for Axle.
+FOUNDATION_EXPORT const unsigned char AxleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Axle/PublicHeader.h>
+
+
